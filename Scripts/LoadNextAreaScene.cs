85be46e1b15fd0ac75b31805c3175a17580f26a2
ReeceMcintyre <ReeceMcintyre998@gmail.com>
2017-09-04 01:18:39 +1000

    Add files via upload

---
+++ Scripts/LoadNextAreaScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextAreaScene : MonoBehaviour {
+
+    public string sceneName = string.Empty;
+    public GameObject Player;
+
+    void OnTriggerEnter2D(Collider2D other) //needed to use the 2D version of OnTriggerEnter which is OnTriggerEnter2D as well as Collider2D to make it work 
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(sceneName); //load the scene taking a paramater of a string which will be the name of the scene to load
+        }
+    }
+}
